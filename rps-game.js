bd686f857347b1915b049a9b1712562c07ab92c5
Vera Childs <141092487+boscopup@users.noreply.github.com>
2024-06-12 15:46:33 -0500

    Add playRound function and supporting functions
    The playRound function calls function createWinLoseTieMessage, which
    calls function capitalize. Currently the playRound function can only
    be used from the console. It is not yet called within the code.

--- rps-game.js
+++ rps-game.js
@@ -67,3 +67,82 @@ function getHumanChoice() {
     // Return valid choice
     return choice;
 }
+
+/**
+ * Function playRound
+ * Params: string computerChoice, string humanChoice
+ * Returns: Win/Lose/Tie message
+ * 
+ * Updates the global computerChoice or humanChoice variables.
+ * 
+ * Rock beats scissors
+ * Paper beats rock
+ * Scissors beats paper
+ * 
+ */
+function playRound(computerChoice, humanChoice) {
+    // If they are the same, the game is a tie
+    if (computerChoice == humanChoice) {
+        // Return a tied message
+        return createWinLoseTieMessage("tie", computerChoice, humanChoice);
+    }
+
+    // if computerChoice is rock and humanChoice is scissors or
+    //    computerChoice is paper and humanChoice is rock or
+    //    comoputerChoice is scissors and humanChoice is paper
+    if (((computerChoice == "rock") && (humanChoice == "scissors")) ||
+        ((computerChoice == "paper") && (humanChoice == "rock")) ||
+        ((computerChoice == "scissors") && (humanChoice == "paper"))) {
+        // Increment computerScore
+        computerScore++;
+
+        // Return computer wins message
+        return createWinLoseTieMessage("lose", computerChoice, humanChoice);
+    } else {
+    // Else
+        // Increment humanScore
+        humanScore++;
+        // Return human wins message
+        return createWinLoseTieMessage("win", humanChoice, computerChoice);
+    }
+}
+
+/**
+ * function createWinLoseTieMessage
+ * Params: string result (win, lose, or tie), 
+ *         string winner (rock, paper, scissors), 
+ *         string loser (rock, paper, scissors)
+ * Returns: string message of the format "You <win|lose|tie>! <<winner> beats <loser>|<winner> ties with <winner>>."
+ * 
+ */
+function createWinLoseTieMessage(result, winner, loser) {
+    // Initialize string message to ""
+    let message = "";
+
+    // Switch on result
+    switch (result) {
+        // If win, return, "You win! <winner capitalized> beats <loser>."
+        // If lose, return, "You lose! <winner capitalized> beats <loser>."
+        case "win":
+        case "lose":
+            message = `You ${result}! ${capitalize(winner)} beats ${loser}.`;
+            break;
+        // If tie, return, "You tie! <winner capitalized> ties with <winner>."
+        case "tie":
+            message = `You tie! ${capitalize(winner)} ties with ${loser}.`;
+            break;
+    }
+
+    // Return the message
+    return message;
+}
+
+/**
+ * function capitalize
+ * Params: string word
+ * Returns: the same string but with the first letter capitalized
+ */
+function capitalize(word) {
+    // Convert the first letter to uppercase and concatenate the rest of the word to that
+    return (word.charAt(0).toUpperCase() + word.slice(1));
+}
