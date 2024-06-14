15a4735587a6f0e160e1577b11b4cc602c6c9230
Vera Childs <141092487+boscopup@users.noreply.github.com>
2024-06-14 11:18:42 -0500

    Add page title and clean up comments in code

--- rps-game.js
+++ rps-game.js
@@ -10,7 +10,6 @@ function getComputerChoice() {
     // Convert 1 to 3 number to string of "rock", "paper", or "scissors"
     stringChoice = convertToRPS(randomNumber);
 
-    // Return string choice
     return stringChoice;
 }
 
@@ -21,41 +20,35 @@ function getComputerChoice() {
  */
 function convertToRPS(number) {
     switch(number) {
-    // if number is 1, return "rock"
     case 1:
         return "rock";
-    // if number is 2, return "paper"
     case 2:
         return "paper";
-    // if number is 3, return "scissors"
     case 3:
         return "scissors";
-    // if number is anything else, return ""
     default:
         return "";
     }
 }
 
 /**
- * Function getHumanChoice
+ * Function: getHumanChoice
  * Params: None
  * Returns: string "rock", "paper", or "scissors"
  */
 function getHumanChoice() {
-    // Initialize stillGoing boolean to true
     let stillGoing = true;
-    // Initialize choice string to an empty string. This will be set in the while loop.
     let choice = "";
 
-    // While stillGoing is true
     while (stillGoing) {
-        // Prompt user for choice of "rock", "paper", or "scissors"
-        choice = prompt("Please choose 'rock', 'paper', or 'scissors'")
+        // Prompt user for choice
+        choice = prompt("Please choose 'rock', 'paper', or 'scissors'");
+
         // Convert choice to all lowercase
         choice = choice.toLowerCase();
-        // Verify that the choice is one of "rock", "paper" or "scissors"
+
+        // Verify choice. If valid, end loop. If not valid, prompt user again.
         if ((choice == "rock") || (choice == "paper") || (choice == "scissors") ) {
-        // If choice is valid, set stillGoing to false and return choice
             stillGoing = false;
         }
     }
@@ -67,7 +60,7 @@ function getHumanChoice() {
 
 
 /**
- * function createWinLoseTieMessage
+ * Function: createWinLoseTieMessage
  * Params: string result (win, lose, or tie), 
  *         string winner (rock, paper, scissors), 
  *         string loser (rock, paper, scissors)
@@ -75,10 +68,8 @@ function getHumanChoice() {
  * 
  */
 function createWinLoseTieMessage(result, winner, loser) {
-    // Initialize string message to ""
     let message = "";
 
-    // Switch on result
     switch (result) {
         // If win, return, "You win! <winner capitalized> beats <loser>."
         // If lose, return, "You lose! <winner capitalized> beats <loser>."
@@ -97,7 +88,7 @@ function createWinLoseTieMessage(result, winner, loser) {
 }
 
 /**
- * function capitalize
+ * Function: capitalize
  * Params: string word
  * Returns: the same string but with the first letter capitalized
  */
@@ -107,17 +98,17 @@ function capitalize(word) {
 }
 
 /**
- * function playGame
+ * Function: playGame
  * Params: None
  * Returns: Nothing 
  */
 function playGame() {
     /**
-     * Function playRound, nested function inside playGame
+     * Function: playRound, nested function inside playGame
      * Params: string computerChoice, string humanChoice
      * Returns: Win/Lose/Tie message
      * 
-     * Updates the global computerChoice or humanChoice variables.
+     * Updates the computerChoice or humanChoice variables.
      * 
      * Rock beats scissors
      * Paper beats rock
@@ -127,49 +118,36 @@ function playGame() {
     function playRound(computerChoice, humanChoice) {
         // If they are the same, the game is a tie
         if (computerChoice == humanChoice) {
-            // Return a tied message
             return createWinLoseTieMessage("tie", computerChoice, humanChoice);
         }
 
-        // if computerChoice is rock and humanChoice is scissors or
-        //    computerChoice is paper and humanChoice is rock or
-        //    comoputerChoice is scissors and humanChoice is paper
+        // If computer wins, send a "lose" message, since the human loses
+        // Otherwise, the human wins, so send a "win" message.
         if (((computerChoice == "rock") && (humanChoice == "scissors")) ||
             ((computerChoice == "paper") && (humanChoice == "rock")) ||
             ((computerChoice == "scissors") && (humanChoice == "paper"))) {
-            // Increment computerScore
             computerScore++;
-
-            // Return computer wins message
             return createWinLoseTieMessage("lose", computerChoice, humanChoice);
         } else {
-        // Else
-            // Increment humanScore
             humanScore++;
-            // Return human wins message
             return createWinLoseTieMessage("win", humanChoice, computerChoice);
         }
     }
     /** END playRound function */
 
-    // Initialize integers computerScore and humanScore to 0
     let computerScore = 0;
     let humanScore = 0;
-    // Initialize strings computerChoice and humanChoice to ""
+
     let computerChoice = "";
     let humanChoice = "";
 
     // Loop for 5 times
     for (let i = 1; i <= 5; i++) {
-        // Get computerChoice
         computerChoice = getComputerChoice();
-
-        // Get humanChoice
         humanChoice = getHumanChoice();
-
-        // Call playRound with computerChoice and humanChoice
         console.log(playRound(computerChoice, humanChoice));
     }
+
     // Compare computerScore and humanScore and declare winner of game
     let message = "";
     if (computerScore > humanScore) {
